4b1f29de7f575132aa5bfb5591a5508fcba9be79
sgigli <simon.luciano@gigli.net>
2020-03-31 23:32:18 -0400

    Updated title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const alwaysOptions = (
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      photo-project-client
+      Photo Project
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
