846506c76e6b331fb92ee3d738dc33fcdc3bb033
sgigli <simon.luciano@gigli.net>
2020-04-20 17:56:41 -0400

    Comments appear in com-section as incoming msgs

--- src/components/App/Modal.js
+++ src/components/App/Modal.js
@@ -15,39 +15,17 @@ function ImageModal (props) {
     return <li key={index}>{comment}</li>
   })
 
-  // <div className="mesgs">
-  //   <div className="msg_history">
-  //     <div className="incoming_msg">
-  //       <div className="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> </div>
-  //       <div className="received_msg">
-  //         <div className="received_withd_msg">
-  //           <p>Test which is a new approach to have all
-  //             solutions</p>
-  //           <span className="time_date"> 11:01 AM    |    June 9</span></div>
-  //       </div>
-  //     </div>
-  //   </div>
-  //   <div className="type_msg">
-  //     <div className="input_msg_write">
-  //       <input type="text" className="write_msg" placeholder="Type a message" />
-  //       <button className="msg_send_btn" type="button"><i className="fa fa-paper-plane-o" aria-hidden="true"></i></button>
-  //     </div>
-  //   </div>
-  // </div>
-
-  // <Row className="show-grid">
-  //   <Col className="modal-img-cont" xs={8}>
-  //     <Image src={props.image.fileUrl} className="modal-image"/>
-  //   </Col>
-  //   <Col className="modal-com-cont">
-  //     <div className="comments">
-  //       Comments
-  //       <ul>
-  //         {comments}
-  //       </ul>
-  //     </div>
-  //   </Col>
-  // </Row>
+  const comments2 = props.image.comments.map((comment, index) => {
+    return <div key={index} className="incoming_msg">
+      <div className="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> </div>
+      <div className="received_msg">
+        <div className="received_withd_msg">
+          <p>{comment}</p>
+          <span className="time_date"> 11:01 AM    |    June 9</span>
+        </div>
+      </div>
+    </div>
+  })
 
   return (
     <div>
@@ -77,9 +55,11 @@ function ImageModal (props) {
                       <div className="received_withd_msg">
                         <p>Test which is a new approach to have all
                           solutions</p>
-                        <span className="time_date"> 11:01 AM    |    June 9</span></div>
+                        <span className="time_date"> 11:01 AM    |    June 9</span>
+                      </div>
                     </div>
                   </div>
+                  {comments2}
                 </div>
                 <div className="type_msg">
                   <div className="input_msg_write">
